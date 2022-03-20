554ef293181c2850f878b69fbbeb1abfc91a6d14
josemiguelhseptien <josemiguelhseptien@gmail.com>
2022-03-20 23:31:02 +0000

    save to submit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+
+  if (randomSuit == "♥" || randomSuit == "♠") {
+    document.querySelector(".container-fluid").style.color = "red";
+  }
+  document.querySelector(".topSuit").innerHTML = randomSuit;
+  document.querySelector(".bottomSuit").innerHTML = randomSuit;
+  document.querySelector(".number").innerHTML = randomNumber;
 };
